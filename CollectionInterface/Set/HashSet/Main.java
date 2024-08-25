8f26e3540800b523699c576e61c1e8dabb459070
Bensoli-Inc <benswambua96@gmail.com>
2024-08-25 10:29:27 +0300

    HashSet

---
+++ CollectionInterface/Set/HashSet/Main.java
@@ -0,0 +1,21 @@
+import java.util.Set;
+import java.util.HashSet;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Set<Integer> nums =  new HashSet<Integer>();
+        nums.add(6);
+        nums.add(5);
+        nums.add(8);
+        nums.add(2);
+        nums.add(6);
+
+
+        for(int n : nums)
+        {
+            System.out.println(n);
+        }
+    }
+}
