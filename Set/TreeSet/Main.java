bfccf6de1954100856f1cb41bb613109b4f892bf
Bensoli-Inc <benswambua96@gmail.com>
2024-08-25 10:52:01 +0300

    Treeset

--- Set/TreeSet/Main.java
+++ Set/TreeSet/Main.java
@@ -1,5 +1,6 @@
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.Iterator;
 
 public class Main
 {
@@ -12,10 +13,16 @@ public class Main
         nums.add(2);
         nums.add(6); // cant have a repeatedvalue
 
+        //USING ITERTOR TO PRINT ELEMENTS
+        Iterator<Integer> values = nums.iterator();
 
-        for(int n : nums)
+        while(values.hasNext())
         {
-            System.out.println(n); // output is sorted. follows sequence
+            System.out.println(values.next());
         }
+        // for(int n : nums)
+        // {
+        //     System.out.println(n); // output is sorted. follows sequence
+        // }
     }
 }
