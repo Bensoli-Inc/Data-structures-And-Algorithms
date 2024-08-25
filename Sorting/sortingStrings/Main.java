d17a97b83b6ad87400c67c7a24ff837ce25e8380
Bensoli-Inc <benswambua96@gmail.com>
2024-08-25 12:22:57 +0300

    sorting multiple data types

--- Sorting/sortingStrings/Main.java
+++ Sorting/sortingStrings/Main.java
@@ -3,37 +3,51 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
+class Student
+{
+    int age;
+    String name;
+
+    public Student(int age, String name)
+    {
+        this.age=age;
+        this.name=name;
+    }
+
+    public String toString()
+    {
+        return "Student [age=" +age + ", name=" + name + "]";
+    }
+}
 
 public class Main
 {
     public static void main(String[] args)
     {
-        List<Integer> nums = new ArrayList<>();
-        nums.add(43);
-        nums.add(31);
-        nums.add(72);
-        nums.add(29);
-
-        //SORTING
-        Collections.sort(nums);
-        System.out.println(nums);
-
-        //SORTING BASED ON OWN LOGIC
-        //EG SORTING BASED ON last digit
-
-        Comparator<Integer> com = new Comparator<Integer>() //COMPARATOR
+        Comparator<Student> com = new Comparator<Student>()
         {
-            public int compare(Integer i, Integer j)
+            public int compare(Student i, Student j)
             {
-                if(i%10 > j%10)
+                if(i.age > j.age)
                     return 1;
                 else
                     return -1;
             }
         };
+
+        List<Student> studs = new ArrayList<>();
+        studs.add(new Student(21, "Ben"));
+        studs.add(new Student(27, "Alex"));
+        studs.add(new Student(19, "Kate"));
+        studs.add(new Student(34, "Mark"));
         
-        Collections.sort(nums, com); 
-        System.out.println(nums);
+
+        //SORTING based on age
+        Collections.sort(studs, com); 
+        for(Student s : studs)
+        {
+            System.out.println(s);
+        }
 
     }
 }
